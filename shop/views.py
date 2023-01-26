24de7571429c18a59de1c05188d82ce05d1e667c
Francois973 <robino.francois973@gmail.com>
2023-01-26 11:33:59 +0100

    chore: add picture with static method in index

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from shop.models import Product
 
 # Create your views here.
+def index(request):
+
+    product = Product.objects.all()
+    return render(request, "shop/index.html", context={"products": product})
--- store/urls.py
+++ store/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import index
+from store import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
