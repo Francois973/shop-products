ac02cde41aa24a40c4231c73a2725b5a6c80bc0c
Francois973 <robino.francois973@gmail.com>
2023-01-26 08:56:41 +0100

    chore: create models and makemigrations

---
+++ shop/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+# Product
+# Name
+# Price
+# Quantity
+# Description
+# Image
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    quantity = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
