a3a19a05c1d286fcc86a5d477718904a28e3dc79
Francois973 <robino.francois973@gmail.com>
2023-01-27 16:58:26 +0100

    chore: install tailwind done

--- store/urls.py
+++ store/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from shop.views import index
 from store import settings
 from django.conf.urls.static import static
+from django.urls import include, path
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
